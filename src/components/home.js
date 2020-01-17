71985dc41ce83111e80aa32ee707367fbfb77980
Tien Pham <tiendev94@gmail.com>
2020-01-17 22:05:04 +0200

    some anchor tags

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function home() {
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <a href="/game" className="btn">Start Game</a>
+      <a href="/game" className="btn">High Scores</a>
+    </div>
+  )
+}
