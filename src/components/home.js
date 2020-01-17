2b97049bfc4830b3e09db66397f45cb5dd9f0f37
Tien Pham <tiendev94@gmail.com>
2020-01-17 22:08:15 +0200

    use home

--- src/components/home.js
+++ src/components/home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function home() {
   return (
-    <div>
+    <div className="container">
       <h1>Quiz App</h1>
       <a href="/game" className="btn">Start Game</a>
       <a href="/game" className="btn">High Scores</a>
