a086cf32a58a59cc404a5226b6fdedc1f4a2d4e8
Tien Pham <tiendev94@gmail.com>
2020-01-18 14:36:53 +0200

    new components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import {Route, BrowserRouter as Router} from 'react-router-dom'
 
 import "./App.css";
 import Home from './components/home'
+import HighScore from './components/HighScore'
+import Game from './components/Game'
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Route path="/" component={Home} />
-      <Route path="/" component={Game} />
-      <Route path="/" component={HighScore} />
+      <Route path="/game" component={Game} />
+      <Route path="/highscore" component={HighScore} />
     </Router>
     </div>
   );
