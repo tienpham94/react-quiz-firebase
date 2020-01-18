da4a1c81daa97bf27c366bf9849fdc86787fea1c
Tien Pham <tiendev94@gmail.com>
2020-01-18 14:34:34 +0200

    More routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+
 import "./App.css";
 import Home from './components/home'
 
 function App() {
   return (
     <div className="App">
-      <Home />
+    <Router>
+      <Route path="/" component={Home} />
+      <Route path="/" component={Game} />
+      <Route path="/" component={HighScore} />
+    </Router>
     </div>
   );
 }
